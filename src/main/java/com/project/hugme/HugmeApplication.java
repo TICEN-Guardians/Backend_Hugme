1f748774eb3384509ac276dcc51ad961971d1ad2
dnwldus410 <dnwldus410@naver.com>
2026-08-04 01:27:45 +0900

    chore: 백엔드 프로젝트 초기 설정

---
+++ src/main/java/com/project/hugme/HugmeApplication.java
@@ -0,0 +1,13 @@
+package com.project.hugme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HugmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HugmeApplication.class, args);
+	}
+
+}
