063f3d183762f6233a0b2f2aee85e2deb044c19a
dnwldus410 <dnwldus410@naver.com>
2026-08-05 10:17:37 +0900

    feat#2:일반회원가입 및 로그인

---
+++ src/main/java/com/project/hugme/domain/auth/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.project.hugme.domain.auth.controller;
+
+import com.project.hugme.domain.auth.dto.LoginRequest;
+import com.project.hugme.domain.auth.dto.LoginResponse;
+import com.project.hugme.domain.auth.dto.SignUpRequest;
+import com.project.hugme.domain.auth.dto.SignUpResponse;
+import com.project.hugme.domain.auth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignUpResponse> signUp(
+            @Valid @RequestBody SignUpRequest req
+            ){
+        SignUpResponse response = authService.signUp(req);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(
+            @Valid @RequestBody LoginRequest request
+    ){
+        LoginResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+
+    }
+
+
+
+
+
+}
--- src/main/java/com/project/hugme/global/security/SecurityConfig.java
+++ src/main/java/com/project/hugme/global/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.project.hugme.global.security;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +26,16 @@ public class SecurityConfig {
 
         return http.build();
 
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService, PasswordEncoder passwordEncoder
+            ){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+
+        provider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(provider);
     }
 }
