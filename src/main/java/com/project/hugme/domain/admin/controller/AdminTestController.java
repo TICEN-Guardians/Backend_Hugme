94dfee4c3e5a3efb2003c27ff823ee6684b1979d
dnwldus410 <dnwldus410@naver.com>
2026-08-08 01:29:53 +0900

    docs: Swagger API 설명 및 태그 추가

--- src/main/java/com/project/hugme/domain/admin/controller/AdminTestController.java
+++ src/main/java/com/project/hugme/domain/admin/controller/AdminTestController.java
@@ -1,5 +1,6 @@
 package com.project.hugme.domain.admin.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/admin")
+@Tag(
+        name = "관리자 API",
+        description = "관리자 권한 API"
+)
 public class AdminTestController {
 
     @GetMapping("/test")
