055b95ff998633a1d0873041c8a97692a6f4795b
ganchisun <minsur0919@gmail.com>
2026-08-11 15:39:59 +0900

    feat: paradeDB 와 postgres(메인DB) datasource 분리

--- src/main/java/com/project/hugme/global/config/ChatbotDataSourceConfig.java
+++ src/main/java/com/project/hugme/global/config/ChatbotDataSourceConfig.java
@@ -1,4 +1,4 @@
-package com.project.hugme.infra.ai.guide;
+package com.project.hugme.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
